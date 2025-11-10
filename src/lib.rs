0d5eda8c2fa66b7b6508ce8b7a435c5ec544a14d
Kishore M <kishorem@Kishores-MacBook-Pro.local>
2025-11-10 12:58:25 +0530

    added update and delete functionality

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use self::models::*;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
+use std::io::stdin;
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
@@ -27,6 +28,81 @@ pub fn display_users(conn: &mut PgConnection) {
     for user in results {
         println!("Id: {}", user.id);
         println!("Name: {}", user.name);
-        println!("Age: {}", user.age);
+        println!("Age: {}\n", user.age);
     }
+}
+
+pub fn create_user(conn: &mut PgConnection) -> User {
+    let mut name: String = String::new();
+    let mut age: String = String::new();
+
+    println!("\nPlease type the name:");
+    stdin()
+        .read_line(&mut name)
+        .expect("Please enter a valid name");
+
+    println!("\nPlease type the age: ");
+    stdin()
+        .read_line(&mut age)
+        .expect("Please enter a valid age");
+
+    let new_age: i32 = age.trim().parse().unwrap();
+
+    let new_user = NewUser {
+        name: &name.trim(),
+        age: new_age,
+    };
+
+    use crate::schema::users;
+
+    diesel::insert_into(users::table)
+        .values(new_user)
+        .returning(User::as_returning())
+        .get_result(conn)
+        .expect("Error creating new user")
+}
+
+pub fn update_user_age(conn: &mut PgConnection) -> User {
+    let mut read_id: String = String::new();
+    let mut read_age = String::new();
+    println!("Please enter the relevant User ID");
+
+    stdin()
+        .read_line(&mut read_id)
+        .expect("Please enter a valid ID");
+
+    let user_id: i32 = read_id.trim().parse().unwrap();
+
+    println!("Please enter the new age: ");
+    stdin()
+        .read_line(&mut read_age)
+        .expect("Please enter a valid age");
+
+    let new_age: i32 = read_age.trim().parse().unwrap();
+
+    use crate::schema::users::dsl::*;
+
+    diesel::update(users.find(user_id))
+        .set(age.eq(new_age))
+        .returning(User::as_returning())
+        .get_result(conn)
+        .expect("Error updating user age")
+}
+
+pub fn delete_user(conn: &mut PgConnection) -> usize {
+    use crate::schema::users::dsl::*;
+
+    let mut read_id = String::new();
+    println!("Please enter the ID of the user you would like to delete");
+    stdin()
+        .read_line(&mut read_id)
+        .expect("Please enter a valid ID number");
+
+    let user_id: i32 = read_id.trim().parse().unwrap();
+
+    let deleted_num = diesel::delete(users.filter(id.eq(user_id)))
+        .execute(conn)
+        .expect("Error deleting user");
+
+    deleted_num
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use diesel_cli_crud::{display_users, establish_connection};
+use diesel_cli_crud::{
+    create_user, delete_user, display_users, establish_connection, update_user_age,
+};
 use std::io::stdin;
 
 fn main() {
@@ -6,8 +8,10 @@ fn main() {
 
     println!("Hello, how would you like to interact with your users table?\n");
     println!("----------");
-    println!("\nPress R if you would like to RETRIEVE the current list of users.\n");
-    println!("Press C if you would like to CREATE a new user.");
+    println!("\nPress R if you would like to RETRIEVE the current list of users\n");
+    println!("Press C if you would like to CREATE a new user\n");
+    println!("Press U to update the age of an existing user\n");
+    println!("Press D to delete an existing user\n");
 
     stdin()
         .read_line(&mut user_input)
@@ -17,6 +21,18 @@ fn main() {
 
     match user_input.as_str().trim() {
         "R" => display_users(connection),
-        _ => println!("WIP"),
+        "C" => {
+            create_user(connection);
+            println!("User added successfully");
+        }
+        "U" => {
+            let user = update_user_age(connection);
+            println!("{}'s age updated successfully to {}", user.name, user.age);
+        }
+        "D" => {
+            let deleted_num = delete_user(connection);
+            println!("{} user deleted successfully", deleted_num);
+        }
+        _ => println!("Please select from one of the valid options. (R/C/U)"),
     }
 }
